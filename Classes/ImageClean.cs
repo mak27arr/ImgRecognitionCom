3a99c760d1856ee263f4c7977e5880713c1822a0
makar <mak27arr@gmail.com>
2019-12-16 17:53:36 +0200

    progres commit

--- Classes/ImageClean.cs
+++ Classes/ImageClean.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
-using System.Text;
 using TextRecognitionComObject.Interface;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace TextRecognitionComObject.Classes
 {
@@ -11,6 +13,7 @@ namespace TextRecognitionComObject.Classes
     {
         public byte[] CleanLowThen(byte[] img,int r,int g,int b)
         {
+            byte[] result;
             MemoryStream buf = new MemoryStream(img);
             Image image = Image.FromStream(buf, true);
             using (Graphics gra = Graphics.FromImage(image))
@@ -26,7 +29,71 @@ namespace TextRecognitionComObject.Classes
                     }
                 }
             }
-            return img;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                image.Save(ms, ImageFormat.Png);
+                result = ms.ToArray();
+            }
+            return result;
+        }
+
+        public byte[] CleanHiThen(byte[] img, int r, int g, int b)
+        {
+            byte[] result;
+            //MemoryStream buf = new MemoryStream(img);
+            //Image image = Image.FromStream(buf, true);
+            //using (Graphics gra = Graphics.FromImage(image))
+            //{
+            //for (int ri = r; ri < 255; ri++)
+            //{
+            //    for (int gi = g; gi < 255; gi++)
+            //    {
+            //        for (int bi = b; bi < 255; bi++)
+            //        {
+            //            gra.Clear(Color.FromArgb(ri, gi, bi));
+            //        }
+            //    }
+            //}
+            //}
+            MemoryStream buf = new MemoryStream(img);
+            Image image = Image.FromStream(buf, true);
+            using (Graphics gra = Graphics.FromImage(image))
+            {
+                Parallel.For(r, 256, ri =>
+                {
+                    Parallel.For(g, 256, gi =>
+                    {
+                        for (int bi = b; bi < 255; bi++)
+                        {
+                            gra.Clear(Color.FromArgb(ri, gi, bi));
+                        }
+                    });
+                });
+            }
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                image.Save(ms, ImageFormat.Png);
+                result = ms.ToArray();
+            }
+            return result;
+        }
+
+        public byte[] DetectImageRealSize(byte[] img,int retreat = 0)
+        {
+            byte[] result;
+            MemoryStream buf = new MemoryStream(img);
+            Image image = Image.FromStream(buf, true);
+            using (Graphics gra = Graphics.FromImage(image))
+            {
+                //gra.
+            }
+            using (MemoryStream ms = new MemoryStream())
+            {
+                image.Save(ms, ImageFormat.Png);
+                result = ms.ToArray();
+            }
+            return result;
         }
     }
 }
