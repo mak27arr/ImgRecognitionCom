fb8c676f3c63c3faa7518805673f3903ddec881f
andriy makar <mak27arr@gmail.com>
2019-12-19 00:37:19 +0200

    reggex add

---
+++ Classes/Regex.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TextRecognitionComObject.Classes
+{
+    public class Regex
+    {
+        public string Replace(string str,string pattern,string symbol = "")
+        {
+            Regex r = new Regex();
+            return r.Replace(str, pattern, symbol);
+        }
+        public string Match(string str, string pattern, string symbol = "")
+        {
+            Regex r = new Regex();
+            return r.Match(str, pattern, symbol);
+        }
+    }
+}
